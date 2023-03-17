95e60d3ceb3a83d674785df0aa47d97e3f665c70
maxim <milovanov160386@gmail.com>
2023-03-17 15:08:58 +0300

    write EmailClient::send_email func and test

--- src/configuration.rs
+++ src/configuration.rs
@@ -20,7 +20,8 @@ pub struct Settings {
 #[derive(serde::Deserialize)]
 pub struct EmailClientSettings {
     pub base_url: String,
-    pub sender_email: String
+    pub sender_email: String,
+    pub authorization_token: Secret<String>
 }
 
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -125,7 +125,8 @@ async fn spawn_app() -> TestApp {
         .expect("Invalid sender email address.");
     let email_client = EmailClient::new(
         configuration.email_client.base_url,
-        sender_email
+        sender_email,
+        configuration.email_client.authorization_token
     );
     let server = zero2prod::startup::run(listener, connection_pool.clone(), email_client)
         .expect("Failed to bind address");
