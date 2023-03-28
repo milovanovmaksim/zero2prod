061a58e626a729bd4948394f49a7f12073491f85
milovanovmaksim <milovanov160386@gmail.com>
2023-03-28 19:24:29 +0300

    spawn blocking with tracing

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,3 +1,4 @@
+use crate::telemetry::spawn_blocking_with_tracing;
 use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
 use actix_web::http::{header, StatusCode};
 use actix_web::HttpRequest;
@@ -83,7 +84,7 @@ async fn validate_credentials(
         .await
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username")))?;
-    tokio::task::spawn_blocking(move || {
+    spawn_blocking_with_tracing(move || {
         verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
