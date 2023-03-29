7f21bfc87ef837db84f342ac9508268a8244a7cf
milovanovmaksim <milovanov160386@gmail.com>
2023-03-29 20:01:07 +0300

    added post method for /login route

--- src/startup.rs
+++ src/startup.rs
@@ -6,7 +6,9 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 
 pub struct Application {
     port: u16,
@@ -75,6 +77,7 @@ pub fn run(
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
@@ -83,4 +86,3 @@ pub fn run(
     .run();
     Ok(server)
 }
-
